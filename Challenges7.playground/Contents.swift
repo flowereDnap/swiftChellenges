9200f1f546b9ac8f0e569ea86636b30150884971
flowereDnap <nikitashapko5@gmail.com>
2021-08-30 19:37:21 +0300

    7 commented

--- Challenges7.playground/Contents.swift
+++ Challenges7.playground/Contents.swift
@@ -21,75 +21,72 @@ import UIKit
  12. array5 += 6                -
  13. for item in array5 { print(item) }     +
  »
-
+ 
  Отрывок из книги: By Ray Fix. «Swift Apprentice». Apple Books.
+ */
+/*
+ //2
+ func removingOnce(_ item: Int, from array: [Int]) -> [Int]
+ {
  
-
-//2
-func removingOnce(_ item: Int, from array: [Int]) -> [Int]
-{
-    
-    guard let index = array.firstIndex(of: item) else {
-        return array
-    }
-    var ret_arr = array
-    ret_arr.remove(at: index )
-    return ret_arr
-}
-
-//3
-func removing(_ item: Int, from array: [Int]) -> [Int]
-{
-    var ret_array = array
-    ret_array.removeAll(where: {$0 == item})
-    return ret_array
-}
-
-//4
-func reversed(_ array: [Int]) -> [Int]
-{
-    var rev_array: [Int] = []
-    
-    for el in array
-    {
-        rev_array.insert(el, at: 0)
-    }
-    return rev_array
-}
-
-//5
-func middle(_ array: [Int]) -> Int?
-{
-    let middle_index = Int(array.count/2)
-    
-    let ans: Int? = array[middle_index]
-    return ans
-}
+ guard let index = array.firstIndex(of: item) else {
+ return array
+ }
+ var ret_arr = array
+ ret_arr.remove(at: index )
+ return ret_arr
+ }
  
+ //3
+ func removing(_ item: Int, from array: [Int]) -> [Int]
+ {
+ var ret_array = array
+ ret_array.removeAll(where: {$0 == item})
+ return ret_array
+ }
+ 
+ //4
+ func reversed(_ array: [Int]) -> [Int]
+ {
+ var rev_array: [Int] = []
+ 
+ for el in array
+ {
+ rev_array.insert(el, at: 0)
+ }
+ return rev_array
+ }
+ 
+ //5
+ func middle(_ array: [Int]) -> Int?
+ {
+ let middle_index = Int(array.count/2)
+ 
+ let ans: Int? = array[middle_index]
+ return ans
+ }
+ */
 //6
-func minMax(of numbers: [Int]) -> (min: Int, max: Int)?
-{
+
+func minMax(of numbers: [Int]) -> (min: Int, max: Int)? {
     var ans: (min: Int, max: Int)?
-    if numbers.isEmpty{
+    if numbers.isEmpty {
         return ans
-    }
-    else{
-        ans=(numbers.first!, numbers.first!)
-    for num in numbers {
-        if num > ans!.max { ans!.max = num}
-        if num < ans!.min { ans!.min = num}
+    } else {
+        ans = (numbers.first!, numbers.first!)
+        for num in numbers {
+            if num > ans!.max { ans!.max = num}
+            if num < ans!.min { ans!.min = num}
         }
     }
     return ans
 }
 let test: [Int] = []
 print(minMax(of:test))
- «1. let dict1: [Int, Int] = [:]
- 2. let dict2 = [:]
- 3. let dict3: [Int: Int] = [:]»
 
- Отрывок из книги: By Ray Fix. «Swift Apprentice». Apple Books.
-//7
+
+/*
+ //7
  /*
  «Which of the following are valid statements?
  1. let dict1: [Int, Int] = [:]     -
@@ -107,79 +104,79 @@ print(minMax(of:test))
  9. dict5["WA"] = "Washington"  +
  10. dict5["CA"] = nil          +
  »
-
+ 
  Отрывок из книги: By Ray Fix. «Swift Apprentice». Apple Books.
  */
-
-
-
-//8
-let dict: [String:String] = ["NY": "New York", "CA": "California"]
-
-func longNames (_ dict: [String:String]) -> [String]
-{
-    var output: [String] = []
-    
-    for (_ , value) in dict
-    {
-        var lenth = 0
-        for char in value{
-            if char != " "
-            {
-                lenth += 1
-            }
-        }
-        if lenth >= 8
-        {
-            output.append(value)
-        }
-    }
-    return output
-}
-
-print ( longNames(dict))
-
-//9
-func merging(_ dict1: [String: String], with dict2: [String: String]) -> [String: String]
-{
-    var output:[String: String] = [:]
-    
-    for (key , value) in dict1{
-        output[key] = value
-    }
-    for (key , value) in dict2{
-        output[key] = value
-    }
-    return output
-}
-
-//10
-func occurrencesOfCharacters(in text: String) -> [Character: Int]
-{
-    var output: [Character: Int] = [:]
-    
-    for char in text{
-        output[char, default: 0] += 1
-    }
-    return output
-}
-
-//11
-func isInvertible(_ dictionary: [String: Int]) -> Bool{
-    let tst_set = Set(dictionary.values)
-    if tst_set.count == dictionary.count
-    {
-        return true
-    }
-    else{ return false}
-}
-*/
-//12
-var nameTitleLookup: [String: String?] = ["Mary": "Engineer", "Patrick": "Intern", "Ray": "Hacker"]
-
-nameTitleLookup["Patrick"] = nil
-nameTitleLookup.remove(at: nameTitleLookup.index(forKey: "Ray")!)
-
-print(nameTitleLookup)
-
-
+ 
+ 
+ 
+ //8
+ let dict: [String:String] = ["NY": "New York", "CA": "California"]
+ 
+ func longNames (_ dict: [String:String]) -> [String]
+ {
+ var output: [String] = []
+ 
+ for (_ , value) in dict
+ {
+ var lenth = 0
+ for char in value{
+ if char != " "
+ {
+ lenth += 1
+ }
+ }
+ if lenth >= 8
+ {
+ output.append(value)
+ }
+ }
+ return output
+ }
+ 
+ print ( longNames(dict))
+ 
+ //9
+ func merging(_ dict1: [String: String], with dict2: [String: String]) -> [String: String]
+ {
+ var output:[String: String] = [:]
+ 
+ for (key , value) in dict1{
+ output[key] = value
+ }
+ for (key , value) in dict2{
+ output[key] = value
+ }
+ return output
+ }
+ 
+ //10
+ func occurrencesOfCharacters(in text: String) -> [Character: Int]
+ {
+ var output: [Character: Int] = [:]
+ 
+ for char in text{
+ output[char, default: 0] += 1
+ }
+ return output
+ }
+ 
+ //11
+ func isInvertible(_ dictionary: [String: Int]) -> Bool{
+ let tst_set = Set(dictionary.values)
+ if tst_set.count == dictionary.count
+ {
+ return true
+ }
+ else{ return false}
+ }
+ 
+ //12
+ var nameTitleLookup: [String: String?] = ["Mary": "Engineer", "Patrick": "Intern", "Ray": "Hacker"]
+ 
+ nameTitleLookup["Patrick"] = nil
+ nameTitleLookup.remove(at: nameTitleLookup.index(forKey: "Ray")!)
+ 
+ print(nameTitleLookup)
+ 
+ */
