ce96df7cf6ac0b129e71149cb4535ad641bd9959
flowereDnap <nikitashapko5@gmail.com>
2021-10-11 20:16:06 +0300

    added 21

--- Challenges21.playground/Contents.swift
+++ Challenges21.playground/Contents.swift
@@ -1,3 +1,28 @@
 import UIKit
 
-var greeting = "Hello, playground"
+enum myErrors: Error{
+    case not_valid_string
+    case not_devideble
+}
+
+
+func to_even_int (string:String) throws -> Int  {
+    guard let number = Int(string) else {
+        throw myErrors.not_valid_string
+    }
+    return (number - number%2)
+    
+}
+
+do{
+    print(try to_even_int(string: "4.5"))
+}catch myErrors.not_valid_string{
+    print("bad string")
+}
+
+func devision(first:Int ,second:Int) throws -> Int{
+    guard let result = first/second else {
+        throw myErrors.not_devideble
+    }
+    return result
+}
